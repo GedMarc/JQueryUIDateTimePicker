daa1b4ebc486ad39526dfae534818a6a6a82ebcf
Marc Magon <ged_marc@hotmail.com>
2017-10-16 21:20:58 +0200

    Updates and preparation for .12
    Signed-off-by: Marc Magon <ged_marc@hotmail.com>

--- test/za/co/mmagon/jwebswing/plugins/jqueryuidatetimepicker/JQueryUIDateTimePickerTest.java
+++ test/za/co/mmagon/jwebswing/plugins/jqueryuidatetimepicker/JQueryUIDateTimePickerTest.java
@@ -1,7 +1,7 @@
 package za.co.mmagon.jwebswing.plugins.jqueryuidatetimepicker;
 
 import org.junit.jupiter.api.Test;
-import za.co.mmagon.BaseTestClass;
+import za.co.mmagon.jwebswing.BaseTestClass;
 import za.co.mmagon.jwebswing.Page;
 
 public class JQueryUIDateTimePickerTest extends BaseTestClass
