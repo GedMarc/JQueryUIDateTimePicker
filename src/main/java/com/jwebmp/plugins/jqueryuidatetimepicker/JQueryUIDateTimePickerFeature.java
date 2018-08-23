91fcfbecd758faed2f624c76e27df53be5b4c920
GedMarc <ged_marc@hotmail.com>
2018-08-23 12:22:21 +0200

    A little bit more type safe, pre configure on component add, performance

--- src/main/java/com/jwebmp/plugins/jqueryuidatetimepicker/JQueryUIDateTimePickerFeature.java
+++ src/main/java/com/jwebmp/plugins/jqueryuidatetimepicker/JQueryUIDateTimePickerFeature.java
@@ -28,8 +28,7 @@ import com.jwebmp.core.base.html.interfaces.GlobalFeatures;
  * @since 2013/01/16
  */
 public class JQueryUIDateTimePickerFeature
-		extends Feature<JQueryUIDateTimePickerOptions, JQueryUIDateTimePickerFeature>
-		implements JQueryUIDateTimePickerFeatures, GlobalFeatures
+		extends Feature<GlobalFeatures, JQueryUIDateTimePickerOptions, JQueryUIDateTimePickerFeature>
 {
 
 	private static final long serialVersionUID = 1L;
@@ -51,26 +50,12 @@ public class JQueryUIDateTimePickerFeature
 	@Override
 	public int hashCode()
 	{
-		int hash = 7;
-		hash = 79 * hash + (getID().hashCode());
-		return hash;
+		return super.hashCode();
 	}
 
 	@Override
 	public boolean equals(Object obj)
 	{
-		if (this == obj)
-		{
-			return true;
-		}
-		if (obj == null)
-		{
-			return false;
-		}
-		if (getClass() != obj.getClass())
-		{
-			return false;
-		}
 		return super.equals(obj);
 	}
 
