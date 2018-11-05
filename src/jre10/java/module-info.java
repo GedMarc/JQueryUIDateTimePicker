68be31fd9876b5e6f86a9ccb9b525101b3ee6533
GedMarc <ged_marc@hotmail.com>
2018-11-05 21:39:41 +0200

    Inclusions and Exclusions, base package change to correct for jquery ui themes, bootstrap tags update, smart wizard split for bootstrap 4

--- src/jre10/java/module-info.java
+++ src/jre10/java/module-info.java
@@ -1,4 +1,6 @@
 import com.jwebmp.core.services.IPageConfigurator;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanJarExclusions;
+import com.jwebmp.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 import com.jwebmp.plugins.jqueryuidatetimepicker.JQueryUIDateTimePickerPageConfigurator;
 
 module com.jwebmp.plugins.jqueryuidatetimepicker {
@@ -11,7 +13,12 @@ module com.jwebmp.plugins.jqueryuidatetimepicker {
 	requires java.validation;
 	requires java.logging;
 	requires com.jwebmp.plugins.bootstrap;
+	requires com.jwebmp.guicedinjection;
 
 	provides IPageConfigurator with JQueryUIDateTimePickerPageConfigurator;
-	opens com.jwebmp.plugins.jqueryuidatetimepicker to com.fasterxml.jackson.databind,com.jwebmp.core;
+
+	provides IGuiceScanJarExclusions with com.jwebmp.plugins.jqueryuidatetimepicker.implementations.JQueryUIDateTimePickerExclusionsModule;
+	provides IGuiceScanModuleExclusions with com.jwebmp.plugins.jqueryuidatetimepicker.implementations.JQueryUIDateTimePickerExclusionsModule;
+
+	opens com.jwebmp.plugins.jqueryuidatetimepicker to com.fasterxml.jackson.databind, com.jwebmp.core;
 }
